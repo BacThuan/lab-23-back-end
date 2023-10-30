9d9767745f8062f1610ebda7e5adefbf39fd3777
BacThuan <120576157+BacThuan@users.noreply.github.com>
2023-10-30 07:50:42 +0700

    Update app.js port

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    const server = app.listen(process.env.PORT || 5000);
+    const server = app.listen(process.env.PORT || 3000);
     console.log("Connected!");
     const io = require("./socket").init(server);
     io.on("connection", (socket) => {
